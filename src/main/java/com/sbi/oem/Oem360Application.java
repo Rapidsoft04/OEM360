999a1dba8665a68b70703ff02a8ec489ba49b558
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2023-12-18 11:44:18 +0530

    @initial commit
    jwt token authentication and authorization added.

---
+++ src/main/java/com/sbi/oem/Oem360Application.java
@@ -0,0 +1,13 @@
+package com.sbi.oem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oem360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oem360Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/sbi/oem/service/UserService.java
@@ -0,0 +1,17 @@
+package com.sbi.oem.service;
+
+import com.sbi.oem.dto.ForgetPasswordRequestDto;
+import com.sbi.oem.dto.LoginRequest;
+import com.sbi.oem.dto.LoginResponse;
+import com.sbi.oem.dto.Response;
+import com.sbi.oem.dto.SignUpRequest;
+
+public interface UserService {
+
+	LoginResponse login(LoginRequest loginRequest) throws Exception;
+
+	Response<?> registerUser(SignUpRequest signUpRequest);
+
+	Response<?> forgetPassword(ForgetPasswordRequestDto forgetPassword);
+
+}
