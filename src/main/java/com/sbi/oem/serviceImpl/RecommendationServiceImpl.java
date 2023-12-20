8591068ef98547e66e60deae35d635c8f3a6f6c8
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2023-12-20 18:56:26 +0530

    @commited after merge

--- src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
+++ src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
@@ -373,7 +373,7 @@ public class RecommendationServiceImpl implements RecommendationService {
 				if (recommendDeploymentDetails != null && recommendDeploymentDetails.isPresent()) {
 					deplyomentDetailsRepository.delete(recommendDeploymentDetails.get());
 				}
-				notificationService.save(recommendObj.get(), RecommendationStatusEnum.REJECTED);
+				notificationService.save(recommendObj.get(), RecommendationStatusEnum.REJECTED_BY_APPOWNER);
 				return new Response<>(HttpStatus.OK.value(), "Recommendation rejected successfully.", null);
 			} else {
 				return new Response<>(HttpStatus.BAD_REQUEST.value(), "You have no access to reject.", null);
