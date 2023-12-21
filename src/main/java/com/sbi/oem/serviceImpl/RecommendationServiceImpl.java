56e22e33002b9dc420391c5ec21bf572de0c284b
Sandeep-Balli-Rapidsoft <sandeepballi.rapidsoft@gmail.com>
2023-12-21 17:00:50 +0530

    Optimized Notification Service

--- src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
+++ src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
@@ -446,7 +446,6 @@ public class RecommendationServiceImpl implements RecommendationService {
 						messages.setCreatedAt(new Date());
 						recommendationMessagesRepository.save(messages);
 						notificationService.save(recommendObj.get(), RecommendationStatusEnum.REJECT_RECOMMENDATION);
-						// Test
 						return new Response<>(HttpStatus.OK.value(), "Recommendation rejected successfully.", null);
 					}
 				} else {
