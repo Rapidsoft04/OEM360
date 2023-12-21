da751800d0a9762ba37f35b681c4c6b4b1f3409c
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2023-12-21 10:40:49 +0530

    @changes in deploymentdetails update

--- src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
+++ src/main/java/com/sbi/oem/serviceImpl/RecommendationServiceImpl.java
@@ -505,6 +505,12 @@ public class RecommendationServiceImpl implements RecommendationService {
 							.findByReferenceId(details.getRecommendRefId());
 					recommendation.get().setExpectedImpact(recommendationDetailsRequestDto.getImpactedDepartment());
 					recommendationRepository.save(recommendation.get());
+					RecommendationMessages messages=new RecommendationMessages();
+					messages.setAdditionalMessage(recommendationDetailsRequestDto.getDescription());
+					messages.setCreatedBy(recommendationDetailsRequestDto.getCreatedBy());
+					messages.setCreatedAt(new Date());
+					messages.setReferenceId(recommendationDetailsRequestDto.getRecommendRefId());
+					recommendationMessagesRepository.save(messages);
 					return new Response<>(HttpStatus.BAD_REQUEST.value(), "Deployment details updated successfully.",
 							null);
 				} else {
