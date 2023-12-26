a44b6d9e489527f3ad2a2ea1aa57618d44800346
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2023-12-26 16:21:18 +0530

    @order by updated at descending

--- src/main/java/com/sbi/oem/repository/RecommendationRepository.java
+++ src/main/java/com/sbi/oem/repository/RecommendationRepository.java
@@ -17,7 +17,7 @@ public interface RecommendationRepository extends JpaRepository<Recommendation, 
 	@Query(value = "SELECT * FROM recommendation where department_id in (?1) order by updated_at desc",nativeQuery = true)
 	List<Recommendation> findAllByDepartmentIdIn(List<Long> departmentIds);
 
-	@Query(value = "SELECT * FROM recommendation where created_by=?1 order by updated_at asc",nativeQuery = true)
+	@Query(value = "SELECT * FROM recommendation where created_by=?1 order by updated_at desc",nativeQuery = true)
 	List<Recommendation> findAllByUserId(Long id);
 
 }
