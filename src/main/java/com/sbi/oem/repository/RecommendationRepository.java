65ecbe55d4fb9b09268e8206da2606230dbd1d28
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2024-01-22 12:10:51 +0530

    @Approved recommendation issue for DGM fixed

--- src/main/java/com/sbi/oem/repository/RecommendationRepository.java
+++ src/main/java/com/sbi/oem/repository/RecommendationRepository.java
@@ -760,7 +760,9 @@ public interface RecommendationRepository extends JpaRepository<Recommendation, 
 			predicates.add(criteriaBuilder.or(criteriaBuilder.equal(root.get("isAgmApproved"), true),
 					criteriaBuilder.equal(root.get("isAgmRejected"), true),
 					criteriaBuilder.equal(root.get("isAppOwnerApproved"), true),
-					criteriaBuilder.equal(root.get("isAppOwnerApproved"), false)));
+					criteriaBuilder.equal(root.get("isAppOwnerApproved"), false),
+					criteriaBuilder.equal(root.get("isAgmApproved"), false),
+					criteriaBuilder.equal(root.get("isAgmRejected"), false)));
 			return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
 		};
 
