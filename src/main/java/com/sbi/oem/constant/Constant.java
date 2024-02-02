e84ff9e7eece8f56521e3ab9c9f064a4b07a76c3
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2024-02-02 10:02:56 +0530

    @Constant keys added for dashboard keys

--- src/main/java/com/sbi/oem/constant/Constant.java
+++ src/main/java/com/sbi/oem/constant/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public static final String APPLICATION_ACCEPTED = "Recommendation accepted";
 	public static final String APPLICATION_REJECTED = "Recommendation rejected";
 	public static final String AGM_OR_DGM_REJECTED = "AGM/DGM rejected";
-	public static final String AGM_OR_DGM_REVERTED= "AGM/DGM reverted";
+	public static final String AGM_OR_DGM_REVERTED = "AGM/DGM reverted";
 	public static final String RECOMMENDATION_CREATED = "Recommendation created";
 	public static final String TODAY = "Today";
 	public static final String YESTERDAY = "Yesterday";
@@ -13,4 +13,11 @@ public class Constant {
 	public static final String THIS_WEEK = "This Week";
 	public static final String LAST_MONTH = "Last Month";
 	public static final String TILL_TODAY = "Till Today";
+	public static final String Completed = "Completed";
+	public static final String Pending_For_Approval = "Pending for approval";
+	public static final String Implementation = "Implementation";
+	public static final String Rejected = "Rejected";
+	public static final String Planned = "Planned";
+	public static final String On_Time = "On time";
+	public static final String Delayed = "Delayed";
 }
--- src/main/java/com/sbi/oem/enums/RequestEnum.java
+++
@@ -1,6 +0,0 @@
-package com.sbi.oem.enums;
-
-public enum RequestEnum {
-	
-	Completed, Pending_For_Approval, Implementation, Rejected, Planned, On_Time, Delayed
-}
