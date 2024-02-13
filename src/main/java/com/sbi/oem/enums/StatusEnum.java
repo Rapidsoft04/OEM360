da8b23b6b1bd5d69fb08727b8f10f3847a50215f
Manoranjan <manoranjan.jena@rapidsoft.co.in>
2024-02-13 22:54:34 +0530

    @filter issue fixed

--- src/main/java/com/sbi/oem/enums/StatusEnum.java
+++ src/main/java/com/sbi/oem/enums/StatusEnum.java
@@ -4,7 +4,7 @@ public enum StatusEnum {
 
 	OEM_recommendation(1L, "OEM recommendation"), Review_process(2L, "Review process"), Approved(3L, "Approved"),
 	Rejected(4L, "Rejected"), Department_implementation(5L, "Department implementation"),
-	UAT_testing(6L, "UAT testing"), Released(7L, "Released"), No_Action(8L, "No action"), Delayed(9L, "Delay"), Released_With_Delay(10L, "Released with delay"),
+	UAT_testing(6L, "UAT testing"), Released(7L, "Released"), No_Action(8L, "No action"), Delayed(9L, "Delayed"), Released_With_Delay(10L, "Released with delayed"),
 	Planned(11L, "Planned"), On_time(12L, "On time");
 
 	private Long id;
